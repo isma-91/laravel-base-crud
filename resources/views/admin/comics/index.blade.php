037a2ff9037d5257423d40adf6ce100b1be68e18
isma-91 <isma91@hotmail.it>
2023-01-21 08:19:35 +0100

    Fix table

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -2,36 +2,37 @@
 
 @section('content')
 
-    <h2>Comics Disponibili</h2>
+    <div class="container">
+        <div class="table-responsive">
+            <table class="table table-striped table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">Titolo</th>
+                        <th scope="col">Descizione</th>
+                        <th scope="col">Prezzo</th>
+                        <th scope="col">Serie</th>
+                        <th scope="col">Tipo</th>
+                        <th scope="col">Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($comics as $comic)
+                        <tr>
+                            <th scope="row">{{ $comic->title }}</th>
+                            <td>{{ $comic->description }}</td>
+                            <td>{{ $comic->price }}</td>
+                            <td>{{ $comic->series }}</td>
+                            <td>{{ $comic->type }}</td>
+                            <td>
+                                <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="btn btn-primary">Visita</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th scope="col">Titolo</th>
-                <th scope="col">Descizione</th>
-                <th scope="col">Link immagine</th>
-                <th scope="col">Prezzo</th>
-                <th scope="col">Serie</th>
-                <th scope="col">Tipo</th>
-                <th scope="col">Link</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($comics as $comic)
-                <tr>
-                    <th scope="row">{{ $comic->title }}</th>
-                    <td>{{ $comic->description }}</td>
-                    <td>{{ $comic->thumb }}</td>
-                    <td>{{ $comic->price }}</td>
-                    <td>{{ $comic->series }}</td>
-                    <td>{{ $comic->type }}</td>
-                    <td>
-                        <a href="{{ route('comics.show', ['comic' => $comic->id]) }}" class="btn btn-primary">Visita</a>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
     <div class="text-center">
         <a href="{{ route('comics.create') }}" class="btn btn-primary">Aggiungi Fumetto</a>
     </div>
