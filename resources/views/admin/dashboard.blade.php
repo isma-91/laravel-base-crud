d9b6a16431ad8a3bad466b69f4602f1666d242d1
isma-91 <isma91@hotmail.it>
2023-01-23 17:03:38 +0100

    Add dashboard page with button for comics

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.base')
 
 @section('content')
-    <h1>Admin Dashboard</h1>
+    <h1 class="text-center">Admin Dashboard</h1>
+    <div class="d-flex justify-content-center">
+        <a href="{{ route('comics.index') }}" class="btn btn-primary">Gestione Comics</a>
+    </div>
 @endsection
