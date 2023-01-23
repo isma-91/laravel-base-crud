7cb7382374d51ab76ed81de3c0ac166aaa638737
isma-91 <isma91@hotmail.it>
2023-01-23 15:09:59 +0100

    Add homepage and Dashboard

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Real Estate</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
+<body>
+    <h1 class="text-center">Homepage</h1>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'PageController@home')->name('home');
+Route::get('/dashboard', 'PageController@dashboard')->name('dashboard');
 
 Route::resource('comics', 'ComicController');
